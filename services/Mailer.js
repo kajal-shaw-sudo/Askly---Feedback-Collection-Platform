4cba0a7846c534a547faba8649120f555608277e
Kajal15Shaw <kajalshaw28b@gmail.com>
2026-02-15 18:10:31 +0530

    Changed SMTP port to 587

--- services/Mailer.js
+++ services/Mailer.js
@@ -9,7 +9,9 @@ class Mailer {
 
   async send() {
     let transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
