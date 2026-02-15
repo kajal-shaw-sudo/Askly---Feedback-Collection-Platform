44eaa2a6a9ad645f7cf5c41adfb4fdd5fc12e0d3
Kajal15Shaw <kajalshaw28b@gmail.com>
2026-02-15 17:53:38 +0530

    Removed SendGrid dependency

--- services/Mailer.js
+++ services/Mailer.js
@@ -1,55 +1,30 @@
-const sendgrid = require('sendgrid');
-const helper = sendgrid.mail;
-const keys = require('../config/keys');
+const nodemailer = require("nodemailer");
 
-class Mailer extends helper.Mail {
-    constructor({ subject, recipients }, content) {
-        super();
+class Mailer {
+  constructor({ subject, recipients }, content) {
+    this.subject = subject;
+    this.recipients = recipients.map(r => r.email);
+    this.content = content;
+  }
 
-        this.sgApi = sendgrid(keys.sendGridKey);
-        this.from_email = new helper.Email('askly1000@gmail.com');
-        this.subject = subject;
-        this.body = new helper.Content('text/html', content);
-        this.recipients = this.formatAddresses(recipients);
+  async send() {
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+      }
+    });
 
-        this.addContent(this.body);
-        this.addClickTracking();
-        this.addRecipients();
-    }
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: this.recipients,
+      subject: this.subject,
+      html: this.content
+    });
 
-    formatAddresses(recipients) {
-        return recipients.map(({ email }) => {
-            return new helper.Email(email);
-        });
-    }
-
-    addClickTracking() {
-        const trackingSettings = new helper.TrackingSettings();
-        const clickTracking = new helper.ClickTracking(true, true);
-
-        trackingSettings.setClickTracking(clickTracking);
-        this.addTrackingSettings(trackingSettings);
-    }
-
-    addRecipients() {
-        const personalize = new helper.Personalization();
-
-        this.recipients.forEach(recipient => {
-            personalize.addTo(recipient);
-        });
-        this.addPersonalization(personalize);
-    }
-
-    async send() {
-        const request = this.sgApi.emptyRequest({
-            method: 'POST',
-            path: '/v3/mail/send',
-            body: this.toJSON()
-        });
-
-        const response = await this.sgApi.API(request);
-        return response;
-    }
+    return { status: "Sent with Nodemailer" };
+  }
 }
 
 module.exports = Mailer;
